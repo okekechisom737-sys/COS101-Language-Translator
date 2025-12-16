86ced0a965d617345d16f7d5f8722ad195771fe6
okekechisom737-sys <okekechisom737@gmail.com>
2025-12-16 23:09:54 +0300

    Create igbo.py

---
+++ igbo.py
@@ -0,0 +1,23 @@
+# igbo.py
+igbo_dictionary = {
+    "hello": "ndeewo",
+    "water": "mmiri",
+    "food": "nri",
+    "house": "ulo",
+    "love": "ihunanya",
+    "man": "nwoke",
+    "woman": "nwanyi",
+    "child": "nwa",
+    "school": "ulo akwukwo",
+    "book": "akwukwo",
+    "sun": "anyanwu",
+    "moon": "onwa",
+    "money": "ego",
+    "friend": "enyi",
+    "father": "nna",
+    "mother": "nne",
+    "road": "uzo",
+    "market": "ahia",
+    "work": "oru",
+    "peace": "udo"
+}
