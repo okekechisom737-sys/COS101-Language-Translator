1a6c5e6538799794948cc986606aa98fce94ad15
BenJosh-ai <joshuabenjaminasa@gmail.com>
2025-12-16 23:56:08 +0300

    Add Hausa dictionary with common words

---
+++ hausa.py
@@ -0,0 +1,23 @@
+# hausa.py
+hausa_dictionary = {
+    "hello": "sannu",
+    "water": "ruwa",
+    "food": "abinci",
+    "house": "gida",
+    "love": "so",
+    "man": "namiji",
+    "woman": "mace",
+    "child": "yaro",
+    "school": "makaranta",
+    "book": "littafi",
+    "sun": "rana",
+    "moon": "wata",
+    "money": "kudi",
+    "friend": "aboki",
+    "father": "uba",
+    "mother": "uwa",
+    "road": "hanya",
+    "market": "kasuwa",
+    "work": "aiki",
+    "peace": "zaman lafiya"
+}
